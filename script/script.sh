2dd8157fdef7a5a6e91782bba6af1a21cf5ea135
Snukne <leonardo.testolin@studenti.univr.it>
2018-04-11 09:02:26 +0200

    Update script with comments

--- script/script.sh
+++ script/script.sh
@@ -3,16 +3,10 @@
 #AVVIO ROSCORE NODO MASTER
 roscore &
 #ASPETTO UN SECONDO PRIMA DI FAR PARTIRE GLI ALTRI NODI
-#IN MODO DA NON AVERE ERRORI NELLA CONNESSIONE
-#AL NODO MASTER PER I NODI FIGLI
 sleep 1
 #AVVIO PRIMO NODO
-#primo_nodo.sh
-#xterm -hold -e bash primo_nodo.sh &
-#AVVIO SECONDO NODO
 gnome-terminal -x sh -c "rosrun homework1 receiveChangeInfo"
+#AVVIO SECONDO NODO
 gnome-terminal -x sh -c "rosrun homework1 printInfo"
-gnome-terminal -x sh -c "rosrun homework1 sendInfo"
-#xterm -hold -e bash secondo_nodo.sh &
 #AVVIO TERZO NODO
-#xterm -hold -e bash terzo_nodo.sh &
+gnome-terminal -x sh -c "rosrun homework1 sendInfo"
